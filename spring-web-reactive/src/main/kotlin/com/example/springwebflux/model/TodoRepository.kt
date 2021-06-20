e9fec4ad318f7942a280e6dc44f1cf32313cae42
koji.lin <koji.lin@linecorp.com>
2021-06-20 09:13:43 +0900

    Fix component

--- spring-web-reactive/src/main/kotlin/com/example/springwebflux/model/TodoRepository.kt
+++ spring-web-reactive/src/main/kotlin/com/example/springwebflux/model/TodoRepository.kt
@@ -3,8 +3,10 @@ package com.example.springwebflux.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
 import java.time.Instant
 
+@Repository
 interface TodoRepository : CoroutineCrudRepository<Todo, Long>
 
 class Todo(@Id var id: Long?, val name: String, @Column("created_at") val createdAt: Instant) {
